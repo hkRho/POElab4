ad917932ed62381552bc47ae5db14702be281899
Hyunkyung Rho <hrho@olin.edu>
2019-10-14 18:57:02 -0400

    Add comments

--- lab4_bangbang/lab4_bangbang.ino
+++ lab4_bangbang/lab4_bangbang.ino
@@ -1,28 +1,32 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(1); // Right Motor
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(2); // Left Motor
 
-int RSpin = 0;
-int LSpin = 0;
+// initialize variable for right side sensor output value
 int RSvalue = 0;
+
+// initialize variable for left side sensor output value
 int LSvalue = 0;
+
+// initialize variable for the motor speed
 int Mspeed = 30;
-int turn_delay = 10;
+
+// initialize variable for determining whether to accept new data or not
+boolean newData = false;
+
+// Override var - true when directly setting motor speeds, and not via PID
+boolean oride = false;
+
+// Motor Speed | Left sensor threshold | Right sensor threshold | Motor override (stop)
+int cvals[4] = {0,900,870,1};
 
 // Serial Input Work
 const byte numChars = 32;
 char caseData = 'p';
 char inputData[numChars]; // an array to store the received data
 
-boolean newData = false;
-
-// Override var - true when directly setting motor speeds, and not via PID
-boolean oride = false;
-// Motor Speed | Left sensor threshold | Right sensor threshold | Motor override (stop)
-int cvals[4] = {0,900,870,1};
 
 void setup() {
   AFMS.begin();
@@ -30,44 +34,52 @@ void setup() {
   Serial.println("<Arduino is ready>");
 }
 
+
 void loop() {
+  // Grab sensor data
   RSvalue = analogRead(A0);
   LSvalue = analogRead(A1);
-  Serial.print("Right Value: ");
-  Serial.print(RSvalue);
-  Serial.print (" | Left Value: ");
-  Serial.println(LSvalue);
+  printSensorOutput(RSvalue, LSvalue); // print sensor output to determine threshold
   
   // Read Serial input, if any
   recvWithEndMarker();
   parseNewData();
+
+  // If override, set motor speed to 0
   if (cvals[3] == 1) {
     Mspeed = 0;
   }
   else {
     Mspeed = cvals[0];
   }
-  
+
+  // move the robot
   setMotorSpeed(Mspeed,Mspeed);
   decideDirection(RSvalue, LSvalue);
 }
 
+/// set motor speeds with the parameter
 void setMotorSpeed(int RMspeed, int LMspeed) {
   rightMotor->setSpeed(RMspeed);
   leftMotor->setSpeed(LMspeed);
 }
 
+
+/// decide the direction to move depending on the RSvalue and LSvalue
 void decideDirection(int RSvalue, int LSvalue) {
+  // go forward
   if (LSvalue > cvals[1] && RSvalue > cvals[2]) {
     Serial.println("Perfectly on line");
     rightMotor->run(FORWARD);
     leftMotor->run(FORWARD);
   }
+  // turn right
   else if (LSvalue > cvals[1] && RSvalue <= cvals[2]) {
     Serial.println("Too left");
     rightMotor->run(RELEASE);
     leftMotor->run(FORWARD);
   }
+  // turn left
   else if (LSvalue <= cvals[1] && RSvalue > cvals[2]) {
     Serial.println("Too right");
     rightMotor->run(FORWARD);
@@ -75,6 +87,8 @@ void decideDirection(int RSvalue, int LSvalue) {
   }
 }
 
+
+/// read all the data from serial input until a new line appears
 void recvWithEndMarker() {
   static byte ndx = 0;
   char endMarker = '\n';
@@ -101,38 +115,36 @@ void recvWithEndMarker() {
   }
 }
 
+
+/// uses serial connection to change the motor speed and right/left sensor threshold and stop/go
 void parseNewData() {
   if (newData == true) {
-    // Debugs input values
-    // Serial.print("This just in ... ");
-    // Serial.println(inputData);
-    
     int zoomy = atoi(inputData);
     newData = false;
+    
     switch(caseData) {
-      case 'm': case 'M':
+      case 'm': case 'M': // change motor speed
         oride = true;
         Serial.println("Motor speed:");
         Serial.println(zoomy);
         cvals[0] = zoomy;
         break;
-      case 'l': case 'L':
+      case 'l': case 'L': // change left sensor threshold
         Serial.print("Set left sensor threshold: ");
         Serial.println(zoomy);
         cvals[1] = zoomy;
-      case 'r': case 'R':
-        Serial.print("Set left sensor threshold: ");
+      case 'r': case 'R': // change right sensor threshold
+        Serial.print("Set right sensor threshold: ");
         Serial.println(zoomy);
         cvals[2] = zoomy;
-      case 's': case 'S':
-        Serial.println("O god o fuck");
+      case 's': case 'S': // stop the robot
         cvals[3] = 1;
         break;
-      case 'g': case 'G':
+      case 'g': case 'G': // move the robot
         Serial.println("Gotta go fast");
         cvals[3] = 0;
         break;
-      default:
+      default:  // in case of error, set the input value to every element of cvals
         Serial.println("Something screwed up.");
         cvals[0] = zoomy;
         cvals[1] = zoomy;
@@ -141,4 +153,13 @@ void parseNewData() {
         break;
     }
   }
+}
+
+
+/// print the IR sensor output values for testing
+void printSensorOutput(int RSvalue, int LSvalue) {
+  Serial.print("Right Value: ");
+  Serial.print(RSvalue);
+  Serial.print (" | Left Value: ");
+  Serial.println(LSvalue);
 }
